d0181e9b213345347ab109feddad642ddec06028
bruno-chavez <darklaier@gmail.com>
2019-11-16 20:45:48 -0300

    Added product service

---
+++ src/app/services/product.service.ts
@@ -0,0 +1,16 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  constructor(private  httpClient: HttpClient) {
+  }
+
+  getProduct(id) {
+    return this.httpClient.get(`http://desafio-ripley-alb-720130574.us-east-2.elb.amazonaws.com/part_number/${id}`,
+      {withCredentials: true});
+  }
+}
